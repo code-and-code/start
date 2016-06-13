3fb6e15bb9c06af3a711e08be81ae057ec004aac
Angelo Biscola <angelo.biscola@gmail.com>
2016-06-13 12:13:52 -0300

    Fix

--- App/Init.php
+++ App/Init.php
@@ -8,17 +8,17 @@ class Init extends Bootstrap
 {
     protected function initRoutes()
     {
-        $ar['index.home']    = ['route' => '/',           'controller' => 'Index', 'action' => 'index'];
-        $ar['index.contact'] = ['route' => '/contact',    'controller' => 'Index', 'action' => 'contact'];
+        $ar['index.home']     = ['route' => '/',              'controller' => 'Index', 'action' => 'index'];
+        $ar['index.contact']  = ['route' => '/contact',       'controller' => 'Index', 'action' => 'contact'];
 
-        $ar['admin']        = ['route' => '/admin',      'controller' => 'Admin', 'auth' =>true, 'action' => 'index'];
+        $ar['admin']          = ['route' => '/admin',         'controller' => 'Admin', 'auth' =>true, 'action' => 'index'];
 
-        $ar['auth']                   = ['route' => '/auth',               'controller' => 'Auth', 'action' => 'getLogin'    ];
-        $ar['auth.login']             = ['route' => '/auth/login',         'controller' => 'Auth', 'action' => 'postLogin'   ];
-        $ar['auth.logout']            = ['route' => '/auth/logout',        'controller' => 'Auth', 'action' => 'logout'      ];
+        $ar['auth.index']     = ['route' => '/auth',          'controller' => 'Auth', 'action' => 'getLogin'    ];
+        $ar['auth.login']     = ['route' => '/auth/login',    'method'=>'POST',       'controller' => 'Auth', 'action' => 'postLogin'   ];
+        $ar['auth.logout']    = ['route' => '/auth/logout',   'controller' => 'Auth', 'action' => 'logout'      ];
 
-        $ar['register']          = ['route' => '/register',      'controller' => 'Auth', 'action' => 'getRegister' ];
-        $ar['register.store']    = ['route' => '/register/store','controller' => 'Auth', 'action' => 'postRegister'];
+        $ar['register']       = ['route' => '/register',      'controller' => 'Auth', 'action' => 'getRegister' ];
+        $ar['register.store'] = ['route' => '/register/store','method'=>'POST','controller' => 'Auth', 'action' => 'postRegister'];
 
         $this->setRoutes($ar);
     }
