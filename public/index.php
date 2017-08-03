8bed20f7f63a9f9ca8aaa64c5acc05568ecb79bc
Angelo Biscola <angelo.biscola@gmail.com>
2017-08-03 17:39:09 -0300

    update

--- public/index.php
+++ public/index.php
@@ -1,17 +1,16 @@
 <?php
 require_once('../vendor/autoload.php');
 
-
 /*
  * version 1
 use App\Init;
 $bootstrap = new Init();
  */
 
-
 /**
  * version 2
  */
+
 Cac\Core\Core::on(config('app.providers'));
 
 
